7896c4c1b3f13ee48855eca71a6cdbb448604171
Chris Burbidge <christopher.burbidge@tooeleschools.org>
2022-07-05 08:45:37 -0600

    Second Commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from './portfolio/portfolio-container';
+import NavigationContainer from './navigation/nav-container';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+        <NavigationContainer />
         <h1>Chris Burbidge Portfolio</h1>
         <div>
           {moment().format('MMMM Do YYYY, h:mm:ss a')}
         </div>
+        <PortfolioContainer />
       </div>
     );
   }
